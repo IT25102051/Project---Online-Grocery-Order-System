d17112721fb9e5ca88fbfbc57005cc97491e3e30
IT25102051 <it25102051@my.sliit.lk>
2026-05-14 16:56:27 +0300

    Updated User and Notification models

--- src/main/java/com/grocery/model/Notification.java
+++ src/main/java/com/grocery/model/Notification.java
@@ -1,4 +1,23 @@
 package com.grocery.model;
 
 public class Notification {
+   private String notificationId;
+   private String userId;
+   private String message;
+   private String status;
+
+   public Notification(String notificationId, String userId, String message, String status) {
+      this.notificationId = notificationId;
+      this.userId = userId;
+      this.message = message;
+      this.status = status;
    }
+
+   public String getNotificationId() {
+      return notificationId;
+   }
+
+   public String toFileString() {
+      return notificationId + "," + userId + "," + message + "," + status;
+   }
+}
